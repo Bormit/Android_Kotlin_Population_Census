b07e0cf2819b570f06d893ac66e5821ac1b2032b
KARPOVTOP <coolxex86@gmail.com>
2022-06-06 00:15:02 +0300

    Добавление определения местоположения пользователя

--- app/src/main/java/ru/mirea/populationcensus/CreateForm.kt
+++ app/src/main/java/ru/mirea/populationcensus/CreateForm.kt
@@ -1,12 +1,25 @@
 package ru.mirea.populationcensus
 
+import android.Manifest
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.app.ActivityCompat.requestPermissions
+
+import android.content.pm.PackageManager
+import android.util.Log
+import android.widget.*
+
+import androidx.core.app.ActivityCompat
+
+
+
 
 class CreateForm : Fragment() {
+    private val locationPermissionCode = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +30,80 @@ class CreateForm : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_form, container, false)
+        val view = inflater.inflate(R.layout.fragment_create_form, container, false)
+
+        val spinnerSex = view.findViewById<Spinner>(R.id.spinnerSex)
+        val spinnerArraySex = resources.getStringArray(R.array.spinnerSex)
+        val spinnerFamily = view.findViewById<Spinner>(R.id.spinnerFamily)
+        val spinnerArrayFamily = resources.getStringArray(R.array.spinnerFamily)
+        val buttonLocation = view.findViewById<ConstraintLayout>(R.id.buttonLocation)
+        val createClc = view.findViewById<TextView>(R.id.createClc)
+
+        val permissions = arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
+
+        buttonLocation.setOnClickListener {
+            if (ActivityCompat.checkSelfPermission(
+                    context!!,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(
+                    context!!,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                requestPermissions(
+                    activity!!, arrayOf(
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ),
+                    locationPermissionCode
+                )
+            } else {
+                (activity as MainActivity?)?.getLocation()
+
+            }
+
+        }
+
+        val arrayAdapterSex =
+            context?.let { ArrayAdapter(it, android.R.layout.simple_spinner_item, spinnerArraySex) }
+        spinnerSex.adapter = arrayAdapterSex
+        spinnerSex.onItemSelectedListener = object :
+
+            AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                if (spinnerArraySex[p2] != "None") {
+                    Toast.makeText(context, spinnerArraySex[p2], Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+
+        }
+
+        val arrayAdapterFamily =
+            context?.let { ArrayAdapter(it, android.R.layout.simple_spinner_item, spinnerArrayFamily) }
+        spinnerFamily.adapter = arrayAdapterFamily
+        spinnerFamily.onItemSelectedListener = object :
+
+            AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                if (spinnerArrayFamily[p2] != "None") {
+                    Toast.makeText(context, spinnerArrayFamily[p2], Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+
+        }
+
+        return view
     }
 
 }
--- app/src/main/java/ru/mirea/populationcensus/MainActivity.kt
+++ app/src/main/java/ru/mirea/populationcensus/MainActivity.kt
@@ -1,18 +1,62 @@
 package ru.mirea.populationcensus
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LocationListener {
+    private lateinit var locationManager: LocationManager
+    private val locationPermissionCode = 2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         supportActionBar?.hide()
+
+
     }
     override fun onDestroy() {
         super.onDestroy()
         val sharedPref = this.getPreferences(MODE_PRIVATE)
         sharedPref.edit().remove("login").apply()
+    }
+    fun getLocation() {
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), locationPermissionCode)
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, this)
+
+    }
+    private fun getAddress(lat: Double, lng: Double): String {
+        val geocoder = Geocoder(this, Locale.getDefault())
+        val list = geocoder.getFromLocation(lat, lng, 1)
+        return list[0].getAddressLine(0)
+    }
+    override fun onLocationChanged(location: Location) {
+        val adress = getAddress(location.latitude, location.longitude)
+        Toast.makeText(this, adress, Toast.LENGTH_SHORT).show()
+    }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == locationPermissionCode) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
