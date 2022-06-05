b84c22f287b40348c534714fdbeee8047bbec901
KARPOVTOP <coolxex86@gmail.com>
2022-06-05 03:13:51 +0300

    Создание регистрации пользователя

--- app/src/main/java/ru/mirea/populationcensus/LoginFragment.kt
+++ app/src/main/java/ru/mirea/populationcensus/LoginFragment.kt
@@ -5,19 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
 
 class LoginFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val buttReg = view.findViewById<TextView>(R.id.logReg)
+        buttReg.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_regFragment)
+        }
+        return view
     }
 
 }
